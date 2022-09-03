ab88c34a9959fc2dd117b0d367dfbad734146189
siam-26 <siamshahriar390@gmail.com>
2022-09-03 17:56:03 +0600

    main news section completed

--- script.js
+++ script.js
@@ -24,8 +24,41 @@ const setting_navbar_news_links = (data) => {
 const categories = (category_id) => {
     fetch(`https://openapi.programming-hero.com/api/news/category/${category_id}`)
         .then(res => res.json())
-        .then(data => console.log(data.data))
+        .then(data => main_news(data.data))
         .catch(error => console.log(error));
 }
 
-navbar_news_links_function();
+const main_news = (data) => {
+    const main_news_div = document.getElementById('main-news-div');
+    main_news_div.innerHTML = '';
+    data.forEach((value) => {
+        const create_div = document.createElement('div');
+        create_div.classList.add('container-div');
+        create_div.classList.add('d-flex');
+        create_div.innerHTML = `
+       
+        <div class="thumbnail">
+            <img class="mt-5 mb-5 ps-5" src="${value.thumbnail_url ? value.thumbnail_url : 'no data found'}">
+        </div>
+        <div class="mt-2">
+        <div class="mt-5 pt-4 ms-5 w-75">
+            <h5 class="fw-bold">${value.title ? value.title : 'no data found'}</h5>
+            <p class="text-muted ellipsis-content mt-2">${value.details ? value.details : 'no data found'}</p>
+        </div>
+
+        <div class="authorName-view ms-5 mt-5">
+            <img class="author-img" src="${value.author.img ? value.author.img : 'no data found'}">
+            <span class="text-secondary ms-1">${value.author.name ? value.author.name : 'no data found'}</span>
+            <span class="ms-5 ps-5"> <i class="fa-regular fa-eye"></i> ${value.total_view ? value.total_view : 'no data found'}</span>
+            <i class="fa-solid fa-arrow-right-long ms-5 ps-5 details-icon-button"></i>
+            
+
+        </div>
+        </div>
+        
+        `
+        main_news_div.appendChild(create_div);
+    })
+}
+
+navbar_news_links_function('Breaking News');
