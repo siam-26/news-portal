c7d58a97796c214a45e26259bb3ab2820c52bb9e
siam-26 <siamshahriar390@gmail.com>
2022-09-03 11:03:59 +0600

    three sections are added including sort view

--- script.js
+++ script.js
@@ -13,10 +13,19 @@ const setting_navbar_news_links = (data) => {
     data.forEach((value) => {
         const create_div = document.createElement('div');
         create_div.innerHTML = `
-        <button onclick="catagories(${value.category_id})" type="button" class="btn text-muted ms-4 me-2">${value.category_name}</button>
-        
+        <button onclick="categories('${value.category_id}')" type="button" class="btn text-muted ms-4 me-2">${value.category_name}</button>
         `
         navbar_news_links_div.appendChild(create_div);
     })
 }
+
+//Categories
+
+const categories = (category_id) => {
+    fetch(`https://openapi.programming-hero.com/api/news/category/${category_id}`)
+        .then(res => res.json())
+        .then(data => console.log(data.data))
+        .catch(error => console.log(error));
+}
+
 navbar_news_links_function();
